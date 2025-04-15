9528e2ad05361cb0ce8ce34ba4144a52125ee0b5
prakashydv01 <pk143lovek@gmail.com>
2025-04-15 16:30:17 +0545

    project buld added

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <h1> sucessfully deploy</h1>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
+      <Routes>
+        <Route path="/" element={<Home />} />
+        
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
